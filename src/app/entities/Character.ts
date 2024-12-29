5cfa3688874d8ca5dd8e7bb91feaa775889fd563
Kate Makarova <kate.makarova@hotmail.com>
2024-12-29 10:47:07 -0500

    game list

--- src/app/entities/Character.ts
+++ src/app/entities/Character.ts
@@ -2,12 +2,16 @@ export class Character {
   id: number;
   name: string;
   is_mine: boolean;
+  unread_posts: number|null;
+  new_episodes: number|null;
   avatar: string | null;
 
-  constructor(id: number, name: string, is_mine: boolean, image: string | null = null) {
+  constructor(id: number, name: string, is_mine: boolean, image: string | null = null, unread_posts: number|null = null, new_episodes: number|null = null) {
     this.id = id;
     this.name = name;
     this.avatar = image;
     this.is_mine = is_mine;
+    this.unread_posts = unread_posts;
+    this.new_episodes = new_episodes;
   }
 }
--- src/app/entities/Game.ts
+++ src/app/entities/Game.ts
@@ -6,7 +6,10 @@ import {Media} from './Media';
 export class Game {
   id: number;
   name: string;
+  total_episodes: number;
   total_posts: number;
+  total_characters: number;
+  total_users: number;
   description: string;
   image: string | null;
   my_characters:Character[]
@@ -17,7 +20,10 @@ export class Game {
   constructor(id: number,
               name: string,
               description: string,
+              totalEpisodeCount: number,
               totalPostCont: number,
+              totalCharacterCount: number,
+              totalUserCount: number,
               myCharacters: Character[],
               fandoms: Fandom[],
               genres: Genre[],
@@ -25,7 +31,10 @@ export class Game {
               image: string | null = null) {
     this.id = id;
     this.name = name;
+    this.total_episodes = totalEpisodeCount;
     this.total_posts = totalPostCont;
+    this.total_characters = totalCharacterCount;
+    this.total_users = totalUserCount;
     this.description = description;
     this.image = image;
     this.my_characters = myCharacters;
