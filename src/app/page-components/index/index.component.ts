0f5f4de89ac877cb29f26179973d337ed92b44ea
Kate Makarova <kate@donkeymob.com>
2025-01-03 14:18:47 -0500

    index page

--- src/app/page-components/index/index.component.ts
+++ src/app/page-components/index/index.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-index',
-  imports: [],
+  imports: [
+  ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
