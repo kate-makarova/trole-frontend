00840ef90eff987d540685ee7bfe2bd4b1a9c557
Kate Makarova <kate@donkeymob.com>
2025-01-17 17:18:40 -0500

    translations

--- src/app/entities/User.ts
+++ src/app/entities/User.ts
@@ -5,12 +5,19 @@ export class User {
   username: string;
   avatar: string|null;
   is_admin: boolean = false;
+  language: string = 'en'
   characters: Character[] = []
 
-  constructor(id: number, username: string, avatar: string|null = null, is_admin: boolean = false, characters: Character[] = []) {
+  constructor(id: number,
+              username: string,
+              avatar: string|null = null,
+              is_admin: boolean = false,
+              language: string = 'en',
+              characters: Character[] = []) {
     this.id = id;
     this.username = username;
     this.avatar = avatar;
+    this.language = language;
     this.characters = characters;
     this.is_admin = is_admin;
   }
