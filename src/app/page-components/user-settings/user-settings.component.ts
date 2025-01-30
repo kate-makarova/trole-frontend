dccc3c8de7f54c1d6f1f6349211fdfd9927b7f96
Kate Makarova <kate@donkeymob.com>
2025-01-30 17:29:53 -0500

    dark css

--- src/app/page-components/user-settings/user-settings.component.ts
+++ src/app/page-components/user-settings/user-settings.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {SimpleEntity} from "../../entities/SimpleEntity";
 
 @Component({
   selector: 'app-user-settings',
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './user-settings.component.html',
   styleUrl: './user-settings.component.css'
 })
 export class UserSettingsComponent {
+  private formBuilder = inject(FormBuilder);
+  form = this.formBuilder.group({
+    theme: ['light', Validators.required],
+  });
 
+  onSubmit() {
+    console.log(this.form.value)
+  }
 }
