5c7a80cddb1160f4ba365ef559bb3397cf2d70f1
Kate Makarova <kate@donkeymob.com>
2025-04-07 17:02:20 -0400

    send invitation

--- src/app/page-components/send-invitation/send-invitation.component.ts
+++ src/app/page-components/send-invitation/send-invitation.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {InvitationService} from "../../services/invitation/invitation.service";
 
 @Component({
   selector: 'app-send-invitation',
-  imports: [],
+    imports: [
+        FormsModule
+    ],
   templateUrl: './send-invitation.component.html',
   styleUrl: './send-invitation.component.css'
 })
 export class SendInvitationComponent {
+  public email: string = '';
 
+  public constructor(private invitationService: InvitationService) {
+  }
+
+  sendInvitation() {
+    if(this.email.length) {
+      this.invitationService.sendInvitation(this.email).subscribe(() => {
+        console.log('Sent')
+      })
+    } else {
+      console.log('Empty email')
+    }
+  }
 }
--- src/app/services/invitation/invitation.service.ts
+++ src/app/services/invitation/invitation.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {APIService} from "../apiservice/apiservice.service";
+import {HttpClient} from "@angular/common/http";
+import {SessionService} from "../session/session.service";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
-export class InvitationService {
+export class InvitationService extends APIService {
 
-  constructor() { }
+  constructor(http: HttpClient, sessionService: SessionService, router: Router) {
+    super(http, sessionService, router);
+  }
+
+  sendInvitation(email: string) {
+    return this.postData<boolean>('invitation-send', {receiver_email: email})
+  }
 }
