b7ae278bdec4344fc8cb945e265d29c49f199d52
Kate Makarova <kate@donkeymob.com>
2025-04-23 14:50:02 -0400

    word counter

--- src/app/components/word-counter/word-counter.component.ts
+++ src/app/components/word-counter/word-counter.component.ts
@@ -1,19 +1,47 @@
-import {Component, OnInit} from '@angular/core';
-import {SCEditorModule} from "sceditor-angular";
+import {Component, Input, OnInit} from '@angular/core';
+import {SceditorComponent, SCEditorModule} from "sceditor-angular";
+import {Observable, of} from "rxjs";
+import {AsyncPipe} from "@angular/common";
 
 
 @Component({
   selector: 'app-word-counter',
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './word-counter.component.html',
   styleUrl: './word-counter.component.css'
 })
 export class WordCounterComponent implements OnInit {
+    @Input('sceditor_initialized') sceditor_initialized: Observable<boolean> = of(false);
+    character_counter$: Observable<number> = of(0);
+    word_counter$: Observable<number> = of(0);
 
 
   ngOnInit() {
-    SCEditorModule.setOnKeyUp('postEditor', (e: any) => {
-      console.log(e)
+    this.sceditor_initialized.subscribe((initialized: boolean) => {
+      if(!initialized) {return}
+
+      SCEditorModule.setOnKeyUp('postEditor', (e: any) => {
+        console.log(e)
+        const t: string = SCEditorModule.getValue('postEditor');
+        console.log(t)
+        this.character_counter$ = of(t.length + 1);
+     //   if(e.code == 'Space') {
+          const word_count: number = this.calculateWorlds(t);
+          this.word_counter$ = of(word_count + 1);
+       // }
+      })
     })
+  }
+
+  calculateWorlds(text: string): number  {
+    let word_counter: number = 0;
+    for (let ch of text) {
+      if(ch == ' ') {
+        word_counter += 1;
+      }
+    }
+    return word_counter;
   }
 }
